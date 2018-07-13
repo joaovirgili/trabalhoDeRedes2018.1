bdb3caf233cbc8efdc1c744b567954887702e9ad
lucasnbp <lucas.natan.prates@hotmail.com>
2018-07-13 19:53:55 -0300

    bora zoar thalles

---
+++ testehora.py
@@ -0,0 +1,49 @@
+import sys
+import datetime
+
+time_tuple = ( 2018, # Year
+                  8, # Month
+                  13, # Day
+                  21, # Hour
+                 0, # Minute
+                  0, # Second
+                  0, # Millisecond
+              )
+
+def _linux_set_time(time_tuple):
+    import ctypes
+    import ctypes.util
+    import time
+
+    # /usr/include/linux/time.h:
+    #
+    # define CLOCK_REALTIME                     0
+    CLOCK_REALTIME = 0
+
+    # /usr/include/time.h
+    #
+    # struct timespec
+    #  {
+    #    __time_t tv_sec;            /* Seconds.  */
+    #    long int tv_nsec;           /* Nanoseconds.  */
+    #  };
+    class timespec(ctypes.Structure):
+        _fields_ = [("tv_sec", ctypes.c_long),
+                    ("tv_nsec", ctypes.c_long)]
+
+    librt = ctypes.CDLL(ctypes.util.find_library("rt"))
+
+    ts = timespec()
+    ts.tv_sec = int( time.mktime( datetime.datetime( *time_tuple[:6]).timetuple() ) )
+    ts.tv_nsec = time_tuple[6] * 1000000 # Millisecond to nanosecond
+
+    # http://linux.die.net/man/3/clock_settime
+    librt.clock_settime(CLOCK_REALTIME, ctypes.byref(ts))
+
+    print ("set time thalles viado")
+
+
+
+_linux_set_time(time_tuple)
+print("rolou?")
+
